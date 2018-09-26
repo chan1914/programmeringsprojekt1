648b6adce323bd5bc424bc5efc8dad013af8bd44
chanthosh <chan1914@edu.eal.dk>
2018-09-26 11:23:35 +0200

    Add project files.

---
+++ FunctionLibrary/Functions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionLibrary
+{
+    public class Functions
+    {
+        public static string DoThis()
+        {
+            return "Result from DoThis";
+        }
+        public static string DoThat()
+        {
+            return "Result from DoThat";
+        }
+        public static string DoSomething(string something)
+        {
+            if (string.IsNullOrEmpty(something))
+                throw new Exception("You need to specify something!");
+
+            return "Result from doing: " + something;
+        }
+        public static int GetTheAnswerToLifeTheUniverseAndEverything()
+        {
+            return 42;
+        }
+    }
+}
---
+++ SmartMenuApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartMenuLibrary;
+
+namespace SmartMenuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Run();
+        }
+
+        private void Run()
+        {
+            SmartMenu menu = new SmartMenu();
+            menu.LoadMenu("MenuSpec.txt");
+            menu.Activate();
+        }
+    }
+}
