4c98d75a61cd22b900b46d356af2053dbcbec597
chanthosh <chan1914@edu.eal.dk>
2018-09-27 15:44:12 +0200

    no changes

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -51,7 +51,7 @@ namespace SmartMenuLibrary
                     
                     
 
-                    if ()
+                    
                     {
                         Console.WriteLine("hej");
                     }
