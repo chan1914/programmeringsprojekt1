e1142e56ba2736c0d0283389432613471e7f8cb9
chanthosh <chan1914@edu.eal.dk>
2018-09-27 09:40:04 +0200

    filehandler er indsat i load menu

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -10,9 +10,22 @@ namespace SmartMenuLibrary
     {
         public void LoadMenu(string path)
         {
-            // Implement ....
-            // hej med dig fjdf
-            Console.WriteLine("hej");
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"c:..\..\MenuSpec.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+                counter++;
+            }
+
+            file.Close();
+            System.Console.WriteLine("There were {0} lines.", counter);
+            // Suspend the screen.  
+            System.Console.ReadLine();
         }
         public void Activate()
         {
